4f3eeef8e5cee35771994eb9e911de02d4bc16a0
Marco Ho <mho122@my.bcit.ca>
2024-11-08 12:40:37 -0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('COMP4964 Lab 7 Part 2 MHO'));
+app.get('/', (req, res) => res.send('COMP4964 Lab 7 Part 2 MHO. Additional message after adding review stage.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
